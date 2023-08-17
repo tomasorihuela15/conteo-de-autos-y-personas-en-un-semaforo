a220b9662724cc87b3c4dd633b5216e1c6f6f741
tomasorihuela15 <142242983+tomasorihuela15@users.noreply.github.com>
2023-08-17 11:52:42 -0600

    commit img

---
+++ Imagenes/Divicionde2numeros.java
@@ -0,0 +1,11 @@
+public class Divicionde2numeros {
+	public static void main(String[] args) {
+        int val1= 10;
+        int val2= 15;
+        float resultado= 0;
+        
+        resultado = val1/val2;
+        System.out.println("El resultado de la divición es:"+resultado);
+               
+}    
+}
